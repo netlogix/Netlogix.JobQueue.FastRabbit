724ba2ec3dc23437cd5aee6edb3060a573302125
Lars Lauger <lars.lauger@netlogix.de>
2021-01-12 09:59:32 +0100

    FEATURE: Exit fast-rabbit loop after 600 seconds
    Original: b9c3a8c670e544c5926448bfe96ddbb983d00782

--- Classes/Loop.php
+++ Classes/Loop.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Netlogix\JobQueue\FastRabbit;
 
 use Neos\Flow\Annotations as Flow;
+use PhpAmqpLib\Exception\AMQPTimeoutException;
 use t3n\JobQueue\RabbitMQ\Queue\RabbitQueue;
 
 /**
@@ -13,17 +14,44 @@ final class Loop
 {
     protected $queue;
 
-    public function __construct(RabbitQueue $queue)
+    /**
+     * Unix timestamp after which the Loop should exit
+     *
+     * @var int|null
+     */
+    protected $exitAfterTimestamp;
+
+    /**
+     * Timeout in seconds when waiting for new messages
+     *
+     * @var int|null
+     */
+    protected $timeout;
+
+    /**
+     * @param RabbitQueue $queue The Queue to watch
+     * @param int $exitAfter Time in seconds after which the loop should exit
+     */
+    public function __construct(RabbitQueue $queue, int $exitAfter = 0)
     {
         $this->queue = $queue;
+        $this->exitAfterTimestamp = $exitAfter > 0 ? time() + $exitAfter : null;
+        $this->timeout = $exitAfter > 0 ? $exitAfter : null;
     }
 
     public function runMessagesOnWorker(Worker $worker)
     {
         $worker->prepare();
         do {
-            $message = $this->queue->waitAndReserve();
-            $worker->executeMessage($message);
+            try {
+                $message = $this->queue->waitAndReserve($this->timeout);
+                $worker->executeMessage($message);
+            } catch (AMQPTimeoutException $e) {
+            }
+
+            if ($this->exitAfterTimestamp !== null && time() >= $this->exitAfterTimestamp) {
+                break;
+            }
         } while (true);
     }
 }
